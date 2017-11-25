81a54e25dbef9bae4956def2685a44bea8587d8e
colorwheeloflife <colorwheeloflife@gmail.com>
2017-11-24 20:51:19 -0800

    Initial commit. Create-React-App setup with basic React-Map-GL example map rendering.

--- src/map.js
+++ src/map.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
-
-const Mapbox = ReactMapboxGl({
-  accessToken: 'pk.eyJ1IjoiY29sb3J3aGVlbCIsImEiOiJjaXZ1MWl0ZjkwMDl6MnRycW5meWF6b3ViIn0.cipbD6ZfnaP4uB766iqKDg'
-});
+import ReactMapGL from 'react-map-gl';
 
 export default class Map extends Component {
   render() {
     return (
-      <Mapbox
-        style="mapbox://styles/mapbox/streets-v9"
-        containerStyle={{
-          height: "100vh",
-          width: "100vw"
+      <ReactMapGL
+        width={400}
+        height={400}
+        latitude={49.2467}
+        longitude={-123.1173}
+        zoom={12}
+        mapboxApiAccessToken={ 'pk.eyJ1IjoiY29sb3J3aGVlbCIsImEiOiJjaXZ1MWl0ZjkwMDl6MnRycW5meWF6b3ViIn0.cipbD6ZfnaP4uB766iqKDg' } 
+        onViewportChange={(viewport) => {
+          const {width, height, latitude, longitude, zoom} = viewport;
+          // Optionally call `setState` and use the state to update the map.
         }}
-        center={ [-123.1173, 49.2467] }
-        zoom={ [12] }>
-      </Mapbox>
-    )  
+      />
+    );
   }
 }
